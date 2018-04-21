c5c28b2ea8fe90c44585661d549aa1d5abc25978
darjwx <darjwx@gmail.com>
2018-04-21 15:17:08 +0200

    HWKeys: Actions: add full brightness action
    * Set the brightness to full/low according to the data of the light sensor
      Day: full brightness
      Night: low brightness
    
    Change-Id: I701e2920cf2f3804820bfc6a2670ecae037fa858

--- java/com/android/internal/util/hwkeys/ActionHandler.java
+++ java/com/android/internal/util/hwkeys/ActionHandler.java
@@ -41,6 +41,10 @@ import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.hardware.input.InputManager;
+import android.hardware.SensorManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.media.session.MediaSessionLegacyHelper;
@@ -57,6 +61,7 @@ import android.os.SystemClock;
 import android.os.UserHandle;
 import android.os.Vibrator;
 import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
 import android.service.wallpaper.WallpaperService;
 import android.text.TextUtils;
 import android.util.Log;
@@ -135,6 +140,7 @@ public class ActionHandler {
     public static final String SYSTEMUI_TASK_SPLIT_SCREEN = "task_split_screen";
     public static final String SYSTEMUI_TASK_ONE_HANDED_MODE_LEFT = "task_one_handed_mode_left";
     public static final String SYSTEMUI_TASK_ONE_HANDED_MODE_RIGHT = "task_one_handed_mode_right";
+    public static final String SYSTEMUI_TASK_BRIGHTNESS_MODE = "task_brightness_mode";
 
     public static final String INTENT_SHOW_POWER_MENU = "action_handler_show_power_menu";
     public static final String INTENT_TOGGLE_SCREENRECORD = "action_handler_toggle_screenrecord";
@@ -149,7 +155,7 @@ public class ActionHandler {
         sDisabledActions.add(SYSTEMUI_TASK_ONE_HANDED_MODE_LEFT);
         sDisabledActions.add(SYSTEMUI_TASK_ONE_HANDED_MODE_RIGHT);
         // we need to make this more reliable when the user tap the partial screenshot button
-        // quickly and more times 
+        // quickly and more times
         sDisabledActions.add(SYSTEMUI_TASK_REGION_SCREENSHOT);
     }
 
@@ -187,7 +193,8 @@ public class ActionHandler {
         OneHandedModeLeft(SYSTEMUI_TASK_ONE_HANDED_MODE_LEFT, SYSTEMUI, "label_action_one_handed_mode_left", "ic_sysbar_one_handed_mode_left"),
         OneHandedModeRight(SYSTEMUI_TASK_ONE_HANDED_MODE_RIGHT, SYSTEMUI, "label_action_one_handed_mode_right", "ic_sysbar_one_handed_mode_right"),
         MediaArrowLeft(SYSTEMUI_TASK_MEDIA_PREVIOUS, SYSTEMUI, "label_action_media_left", "ic_skip_previous"),
-        MediaArrowRight(SYSTEMUI_TASK_MEDIA_NEXT, SYSTEMUI, "label_action_media_right", "ic_skip_next");
+        MediaArrowRight(SYSTEMUI_TASK_MEDIA_NEXT, SYSTEMUI, "label_action_media_right", "ic_skip_next"),
+        BrightnessMode(SYSTEMUI_TASK_BRIGHTNESS_MODE, SYSTEMUI, "label_action_brightness_mode", "ic_brightness_mode");
 
         String mAction;
         String mResPackage;
@@ -228,7 +235,8 @@ public class ActionHandler {
             SystemAction.VolumePanel, SystemAction.ClearNotifications,
             SystemAction.SplitScreen, SystemAction.RegionScreenshot,
             SystemAction.OneHandedModeLeft, SystemAction.OneHandedModeRight,
-            SystemAction.MediaArrowLeft, SystemAction.MediaArrowRight
+            SystemAction.MediaArrowLeft, SystemAction.MediaArrowRight,
+            SystemAction.BrightnessMode
     };
 
     public static class ActionIconResources {
@@ -661,6 +669,8 @@ public class ActionHandler {
         } else if (action.equals(SYSTEMUI_TASK_ONE_HANDED_MODE_RIGHT)) {
 //            toggleOneHandedMode(context, "right");
             return;
+        } else if (action.equals(SYSTEMUI_TASK_BRIGHTNESS_MODE)) {
+            brightnessMode(context);
         }
     }
 
@@ -1039,4 +1049,62 @@ public class ActionHandler {
             Settings.Global.putString(context.getContentResolver(), Settings.Global.SINGLE_HAND_MODE, "");
     }
     */
+
+    public static void brightnessMode(Context context) {
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        SensorManager sm = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        Sensor lightSensor = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
+        SensorEvent event;
+
+        int minValue = pm.getMinimumScreenBrightnessSetting();
+        int maxValue = pm.getMaximumScreenBrightnessSetting();
+        int currentMode = -1;
+        boolean dayLight;
+
+
+        try  {
+        currentMode = android.provider.Settings.System.getInt(
+            context.getContentResolver(),
+            android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE);
+        } catch (SettingNotFoundException e){
+            //Do nothing
+        }
+
+        SensorEventListener lightSensorListener = new SensorEventListener() {
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (event.sensor.getType() == Sensor.TYPE_LIGHT){
+                    float sensorValue = event.values[0];
+
+                    if (sensorValue <= SensorManager.LIGHT_FULLMOON){
+                        pm.setBacklightBrightness(minValue);
+                    } else if (sensorValue >= SensorManager.LIGHT_SUNLIGHT){
+                        pm.setBacklightBrightness(maxValue);
+                    }
+                }
+            }
+
+        };
+
+        /*if (currentMode == 0) {
+            if (dayLight) {
+                pm.setBacklightBrightness(maxValue);
+            } else {
+                pm.setBacklightBrightness(minValue);
+            }
+        } else {*/
+        if (currentMode == 1){
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, "Auto mode enabled", duration);
+            toast.show();
+        }
+    }
+
 }
